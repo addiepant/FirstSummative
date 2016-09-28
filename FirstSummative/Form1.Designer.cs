12d83d75a240226d29bd79d93af68bc697fc9dff
temp <temp>
2016-09-28 13:34:13 -0400

    Add project files.

---
+++ FirstSummative/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace FirstSummative
+{
+    partial class greetingCard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.notificationLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.BackColor = System.Drawing.Color.Goldenrod;
+            this.openButton.Font = new System.Drawing.Font("Gabriola", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openButton.Location = new System.Drawing.Point(75, 185);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(140, 50);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open Horoscope";
+            this.openButton.UseVisualStyleBackColor = false;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // notificationLabel
+            // 
+            this.notificationLabel.AutoSize = true;
+            this.notificationLabel.Font = new System.Drawing.Font("Gabriola", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.notificationLabel.ForeColor = System.Drawing.Color.DarkOrange;
+            this.notificationLabel.Location = new System.Drawing.Point(37, 78);
+            this.notificationLabel.Name = "notificationLabel";
+            this.notificationLabel.Size = new System.Drawing.Size(219, 39);
+            this.notificationLabel.TabIndex = 1;
+            this.notificationLabel.Text = "Your Horscope for the day is in!";
+            // 
+            // greetingCard
+            // 
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.notificationLabel);
+            this.Controls.Add(this.openButton);
+            this.Name = "greetingCard";
+            this.Text = "Horoscope";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Label notificationLabel;
+    }
+}
+
---
+++ FirstSummative/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace FirstSummative
+{
+    public partial class greetingCard : Form
+    {
+        public greetingCard()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            //Set up brushes and Pens, define variables
+            Graphics formGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Gold, 2);
+            SolidBrush drawBrush = new SolidBrush(Color.Gold);
+            SoundPlayer player = new SoundPlayer(Properties.Resources.chime);
+
+            //Hide butons and label
+            openButton.Hide();
+            notificationLabel.Hide();
+
+            //Form Elipses/stars
+            formGraphics.FillEllipse(drawBrush, 246, 55, 5, 5);
+            player.Play();
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 226, 45, 5, 5);
+            player.Play();
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 206, 112, 5, 5);
+            player.Play();
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 236, 123, 5, 5);
+            player.Play();
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 249, 155, 5, 5);
+            player.Play();
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 134, 183, 5, 5);
+            player.Play();
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 77, 210, 5, 5);
+            player.Play();
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 117, 146, 5, 5);
+            player.Play();
+            Thread.Sleep(500);
+
+            //Form Lines
+            formGraphics.DrawLine(drawPen, 246, 55, 226, 45);
+            Thread.Sleep(500);
+            formGraphics.DrawLine(drawPen, 226, 45, 206, 112);
+            Thread.Sleep(500);
+            formGraphics.DrawLine(drawPen, 206, 112, 236, 123);
+            Thread.Sleep(500);
+            formGraphics.DrawLine(drawPen, 236, 123, 249, 155);
+            Thread.Sleep(500);
+            formGraphics.DrawLine(drawPen, 249, 155, 134, 183);
+            Thread.Sleep(500);
+            formGraphics.DrawLine(drawPen, 134, 183, 77, 210);
+            Thread.Sleep(500);
+            formGraphics.DrawLine(drawPen, 77, 210, 117, 146);
+            Thread.Sleep(500);
+            formGraphics.DrawLine(drawPen, 117, 146, 134, 183);
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
